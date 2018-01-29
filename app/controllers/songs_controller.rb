767e99b62fa4494bd0584c4c4cfc603aca37fb97
Nolan Hughes <nhughes987@gmail.com>
2018-01-29 02:58:04 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,7 +7,7 @@ class SongsController < ApplicationController
 
   get '/songs/:slug' do
     binding.pry
-    @song = Song.find_by(params[:slug])
+    @song = Song.find_by_slug(params[:slug])
 
     erb :'songs/show'
   end
