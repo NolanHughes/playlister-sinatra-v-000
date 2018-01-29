f28c170e6dea35e1b2c1e4f2605d0a89c0c5755d
Nolan Hughes <nhughes987@gmail.com>
2018-01-29 04:02:33 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,6 +13,7 @@ class SongsController < ApplicationController
   post '/songs' do
     @song = Song.new(name: params[:name])
     @artist = Artist.create(name: params[:artist_name])
+    @genre = Genre.find_by_id(params[:genre])
     binding.pry
 
     redirect to '/songs/:slug'
