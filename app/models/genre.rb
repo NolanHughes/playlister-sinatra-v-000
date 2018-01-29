236af85f34c88158316e5531872160a0f38b4806
Nolan Hughes <nhughes987@gmail.com>
2018-01-29 02:48:49 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,7 +1,7 @@
 class Genre < ActiveRecord::Base
   has_many :songs, through: :song_genres
   has_many :song_genres
-  has_many :artists, through: :song_genres, :source => :song
+  has_many :artists, through: :songs
 
   def slug
     self.name.downcase.gsub(" ", "-")
